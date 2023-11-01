41ef8ff79e83303e9c556096cec514c678927178
raoawais888 <raoawais888@gmail.com>
2023-11-01 14:41:19 +0500

    awais project setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter , Routes , Route} from "react-router-dom"
 import Home from "./pages/home/Home"
 import Shop from "./pages/Shop/Shop"
+import Register from "./admin/Pages/register/Register"
+import Login from "./admin/Pages/login/Login"
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
 
       </Routes>
     </BrowserRouter>
